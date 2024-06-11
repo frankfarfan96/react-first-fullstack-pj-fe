4e7e21c74036959d88fa11dcca9f3287c618ac22
frank <frankfarfan96@gmail.com>
2024-06-11 22:07:26 +0200

    wip view for AddRomm

--- src/components/room/AddRoom.jsx
+++ src/components/room/AddRoom.jsx
@@ -58,9 +58,62 @@ const AddRoom = () => {
     }
 
     return (
-        <div>
+        <>
+        <section className="containerm, mt-5 mb-5">
+            <div className="row justify-content-center">
 
-        </div>
+                <div className="col-md-8 col-lg-6">
+                    <h2 className="mt-5 mb-2">Add a New Room</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="roomType" className="form-label"> Room Type </label>
+                            <div>
+
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="roomPrice" className="form-label"> Room Price </label>
+                            <input className="form-control"
+                            required
+                            id="roomPrice"
+                            type="number"
+                            name="roomPrice"
+                            value={newRoom.roomPrice}
+                            onChange={handleRoomInputChange}/>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="roomPhoto" className="form-label"> Room Photo </label>
+                            <input 
+                            id="photo"
+                            name="photo"
+                            type="file"
+                            className="form-control"
+                            onChange={handleImageChange}
+                            />
+                            {imagePreview && (
+                                <img src={imagePreview}
+                                alt="Previw Room Photo"
+                                style={{maxWidth: "400px", maxHeight: "400px"}}
+                                className="mb-3" />
+                            )}
+                        </div>
+
+                        <div className="d-grid d-md-flex mt-2">
+                            <button className="btn btn-outline-primary ml-5">
+                                Save Room
+                            </button>
+
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </section>
+
+
+        </>
     )
 }
 
