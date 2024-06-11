403b9ddb4a360ce5b8cb734ed019e65f6e3ccf2e
frank <frankfarfan96@gmail.com>
2024-06-11 10:45:33 +0200

    create class component for addRomm installed axios and bootstraps dependencies and implemented 2 functions in a new util class to interact to our be to addRoom and to get roomTypes

---
+++ src/components/room/AddRoom.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddRoom = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AddRoom;
---
+++ src/components/utils/ApiFunctions.js
@@ -0,0 +1,37 @@
+// file to create functions that we interact with our server BE
+import axios from "axios";
+
+//create our URL
+export const api = axios.create({
+    baseURL :"http://localhost:9192"
+})
+
+export async function addRoom(photo, roomType, roomPrice) {
+    
+    const formData = new FormData();
+    
+    formData.append("photo", photo);
+    formData.append("roomType", roomType);
+    formData.append("roomPrice", roomPrice);
+   
+    const response = await api.post("/rooms/add/new-room", formData);
+
+    if(response.status === 201) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+// This function is for popolete the select control with all the room types we have in our Db
+
+export async function getRoomTypes() {
+    try {
+        const response = await api.get("/rooms/room-types");
+        
+        return response.data;
+    } catch(error) {
+        throw new Error("Error fetching room types");
+    }
+}
