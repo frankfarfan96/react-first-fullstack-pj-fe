5bdd57017bc94ac67f5117f53811706cde2492b8
frank <frankfarfan96@gmail.com>
2024-06-14 13:17:10 +0200

    added api to edit room and to get room by id

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -60,3 +60,25 @@ export async function deleteRoom(roomId) {
         throw new Error(`Error deleting room ${error.message}`)
     }
 }
+
+
+export async function updateRoom(roomId, roomData) {
+    const formData = new FormData();
+    formData.append("roomType", roomData.roomType);
+    formData.append("roomPrice", roomData.roomPrice);
+    formData.append("photo", roomData.photo);
+
+    const response = await api.put(`/rooms/update/${roomId}`);
+
+    return response;
+}
+
+export async function getRoomById(roomId) {
+    try {
+        const result = await api.get(`/rooms/room/${roomId}`);
+        
+        return result.data;
+    } catch (error) {
+        throw new Error(`Error fetching room ${error.message}`);
+    }
+}
