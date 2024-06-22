1d9d61837378862ca315db4347dc1af6758ef65c
frank <frankfarfan96@gmail.com>
2024-06-22 15:14:39 +0200

    WiP creating components for HomePage

---
+++ src/components/common/HotelService.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Header";
+
+const HotelService = () => {
+    return (
+        <>
+            <Container className="mb-2">
+                <Header title={"Our Services"}/>
+                
+            </Container>
+        </>
+    )
+}
+
+export default HotelService;
---
+++ src/components/layout/HeaderMain.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeaderMain = () => {
+    return(
+        <header className="header-banner">
+            <div className="overlay"></div>
+            <div className="animated-texts overlay-content">
+                <h1>
+                    Welcome To <span className="hotel-color">Franky Hotel</span>
+                </h1>
+                <h4>Experience the best Hospitality in Town</h4>
+            </div>
+        </header>
+    )
+}
+
+export default HeaderMain;
