d06e75939e623bf8156e64d914582c0add3d66ed
frank <frankfarfan96@gmail.com>
2024-07-07 15:49:14 +0200

    created new components for registratin, login and profile and added the Api functions for them

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -6,6 +6,16 @@ export const api = axios.create({
     baseURL :"http://localhost:9192"
 })
 
+
+export const getHeader = () => {
+    const token = localStorage.getItem("token");
+
+    return {
+        Authorization : `Bearer ${token}`,
+        "Content-Type" : "application/json" 
+    }
+}
+
 // This function adds a new room to the database
 export async function addRoom(photo, roomType, roomPrice) {
     
@@ -145,4 +155,50 @@ export async function getAvailableRooms(checkInDate, checkOutDate, roomType) {
     console.log(result)
 	return result
 
+}
+
+
+// This function is for registration
+export async function registration(registration) {
+    try {
+        const response = await api.post("/auth/register-user", registration)
+        
+        return response.data;
+    } catch (error) {
+        if(error.response && error.response.data) {
+            throw new Error(error.response.data);
+        } else {
+            throw new Error(`User registration error : ${error.message}`);
+        }
+    }
+
+} 
+
+// This function is for login user
+export async function loginUser(login) {
+    try {
+        const response = await api.post("/auth/login", login);
+
+        if(response.status >= 200 && response.status < 300) {
+            return response.data;
+        } else {
+        return response.data;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+} 
+
+
+export async function getUserProfile(userId, token) {
+    try {
+        const response = await api.get(`users/profile/${userId}`, {
+            headers : getHeader()
+        });
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 }
